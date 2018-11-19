db69c3a6f160b95c701f01f32bd0d589532ab27a
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-20 00:18:13 +0100

    Temporary matcher usage

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const {find} = require('./matcher')
+
 router.get('/', (req, res) => {
   res.status(200).send('OK')
 })
+
+find('Kamil Lelonek').then(console.log)
 
 module.exports = {router}
