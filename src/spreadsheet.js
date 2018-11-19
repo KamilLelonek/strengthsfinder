2a05173c18e813d467273e0038a1a88cb275a5cc
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-19 16:44:32 +0100

    Provide callback for reading spreadsheet

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,21 +1,25 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const async = require('async')
-const fp = require('lodash/fp')
 
 const {SPREADSHEET_ID} = require('../config')
 const {map} = require('./mapper')
 
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
+function read(cb) {
+  async.series(
+    [
+      step => {
+        const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
 
-async.series(
-  [
-    step =>
-      doc.getInfo((_error, {worksheets: [worksheet]}) => {
-        this.sheet = worksheet
-        step()
-      }),
-    step =>
-      this.sheet.getRows({offset: 4}, (_error, rows) => step(rows.map(map)))
-  ],
-  result => console.log(result)
-)
+        doc.getInfo((_error, {worksheets: [worksheet]}) => {
+          this.sheet = worksheet
+          step()
+        })
+      },
+      step =>
+        this.sheet.getRows({offset: 4}, (_error, rows) => step(rows.map(map)))
+    ],
+    result => cb(result)
+  )
+}
+
+module.exports = {read}
