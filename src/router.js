bb4bc9ca0cc4a925bd141b3950ef5c971143092f
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-20 00:53:36 +0100

    Error handling in Router

--- src/router.js
+++ src/router.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const {find} = require('./matcher')
 
 router.get('/', (req, res) => {
-  res.status(200).send('OK')
+  find('Kamil Lelonek')
+    .then(results => res.status(200).send(results))
+    .catch(({message}) => res.status(400).send(message))
 })
-
-find('Kamil Lelonek').then(console.log)
 
 module.exports = {router}
