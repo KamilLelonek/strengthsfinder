8d6e297e6dfec60dd8fecc0ba90d74ed4b312d4d
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-19 16:32:30 +0100

    Export document ID

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
-  HOST: process.env.HOST
+  HOST: process.env.HOST,
+  SPREADSHEET_ID: '18B7tI6tvgqYFeO3PeCV_pSjfUiYQrkJjKZOT0Q4K5YM'
 }
