024d4d44f310576985b484a406ea639f40d69568
Kamil Lelonek <squixy.sln@gmail.com>
2018-11-28 19:26:09 +0100

    Cleanup spreasheet

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,5 +1,6 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const async = require('async')
+const fp = require('lodash/fp')
 
 const {SPREADSHEET_ID} = require('../config')
 const {map} = require('./mapper')
@@ -9,23 +10,21 @@ function read() {
     async.series(
       [
         step => {
-          const doc = new GoogleSpreadsheet(SPREADSHEET_ID)
-
-          doc.getInfo((_error, {worksheets: [worksheet]}) => {
-            this.sheet = worksheet
-            step()
-          })
+          new GoogleSpreadsheet(SPREADSHEET_ID).getInfo(
+            (_error, {worksheets: [worksheet]}) => {
+              this.sheet = worksheet
+              step()
+            }
+          )
         },
         step =>
-          this.sheet.getRows({offset: 4}, (_error, rows) =>
-            step(null, rows.map(map))
-          )
+          this.sheet.getRows({offset: 4}, (_error, rows) => step(null, rows))
       ],
-      (err, [_step1, step2]) => {
+      (err, [_step1, rows]) => {
         if (err) {
           reject(err)
         } else {
-          resolve(step2)
+          fp.pipe([fp.map(map), resolve])(rows)
         }
       }
     )
