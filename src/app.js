79d97a75e8cd715d72da1e96ea57da4eedf73490
Kamil Lelonek <squixy.sln@gmail.com>
2018-12-23 06:40:55 +0100

    Use middlewares directly

--- src/app.js
+++ src/app.js
@@ -2,7 +2,5 @@ require('dotenv').config()
 
 const {HOST, PORT} = require('../config')
 const server = require('./server')
-const {router} = require('./router')
-const cors = require('cors')
 
-server.start(HOST, PORT, [router, cors])
+server.start(HOST, PORT)
--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 const debug = require('debug')('server')
-const fp = require('lodash/fp')
+
+const {router} = require('./router')
+const {cors} = require('./cors')
 
 function logger(host, port) {
   return () => debug('Listening on %s:%d', host, port)
 }
 
-function start(host, port, middlewares) {
-  fp.map(middleware => app.use(middleware))(middlewares)
+function start(host, port) {
+  app.use(cors)
+  app.use(router)
 
   app.listen(port, logger(host, port))
 }
