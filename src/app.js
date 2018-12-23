efbc9fc9294d4919efe98b8f3e6b587d2e223523
Kamil Lelonek <squixy.sln@gmail.com>
2018-12-23 06:36:56 +0100

    Add external CORS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const {HOST, PORT} = require('../config')
 const server = require('./server')
 const {router} = require('./router')
-const {cors} = require('./cors')
+const cors = require('cors')
 
 server.start(HOST, PORT, [router, cors])
