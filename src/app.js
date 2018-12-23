2feec14831b7b10f0745adf02fa99a8121f66c8b
Kamil Lelonek <squixy.sln@gmail.com>
2018-12-23 06:45:15 +0100

    Simplify server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 require('dotenv').config()
 
 const {HOST, PORT} = require('../config')
-const server = require('./server')
+const express = require('express')
+const app = express()
+const debug = require('debug')('server')
 
-server.start(HOST, PORT)
+const {router} = require('./router')
+const {cors} = require('./cors')
+
+function logger(host, port) {
+  return () => debug('Listening on %s:%d', host, port)
+}
+
+app.use(cors)
+app.use(router)
+
+app.listen(PORT, logger(HOST, PORT))
